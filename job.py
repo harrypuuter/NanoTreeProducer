580e0e8922cd84e7c37fc00da41f79239b30ad28
IzaakWN <izaak.neutelings@cern.ch>
2019-02-03 12:45:26 +0100

    change muele to elemu; add tag for output file with TES

--- job.py
+++ job.py
@@ -2,10 +2,7 @@
 import os,sys
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import * 
 from argparse import ArgumentParser
-
-def ensureDir(directory):
-  if not os.path.exists(directory):
-    os.makedirs(directory)
+from checkFiles import ensureDirectory
 
 infiles = "root://cms-xrd-global.cern.ch//store/user/arizzi/Nano01Fall17/DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAOD-94X-Nano01Fall17/180205_160029/0000/test94X_NANO_70.root"
 
@@ -14,7 +11,7 @@ parser.add_argument('-i', '--infiles', dest='infiles', action='store', type=str,
 parser.add_argument('-o', '--outdir',  dest='outdir', action='store', type=str, default="outdir")
 parser.add_argument('-N', '--outfile', dest='outfile', action='store', type=str, default="noname")
 parser.add_argument('-n', '--nchunck', dest='nchunck', action='store', type=int, default='test')
-parser.add_argument('-c', '--channel', dest='channel', action='store', choices=['tautau','mutau','eletau','muele','mumu'], type=str, default='tautau')
+parser.add_argument('-c', '--channel', dest='channel', action='store', choices=['tautau','mutau','eletau','elemu','mumu'], type=str, default='tautau')
 parser.add_argument('-t', '--type',    dest='type', action='store', choices=['data','mc'], default='mc')
 parser.add_argument('-y', '--year',    dest='year', action='store', choices=[2016,2017,2018], type=int, default=2017)
 parser.add_argument('-T', '--tes',     dest='tes', action='store', type=float, default=1.0)
@@ -27,18 +24,16 @@ outdir   = args.outdir
 outfile  = args.outfile
 nchunck  = args.nchunck
 year     = args.year
+tes      = args.tes
 kwargs   = {
-  'year':  args.year,
-  'tes':   args.tes,
+  'year':  year,
+  'tes':   tes,
 }
 
 if isinstance(infiles,str):
   infiles = infiles.split(',')
 
-if channel=='etau':
-  channel = 'eletau'
-elif channel=='elemu':
-  channel = 'muele'
+ensureDirectory(outdir)
 
 dataType = 'mc'
 if infiles[0].find("/SingleMuon/")>0 or infiles[0].find("/Tau/")>0 or infiles[0].find("/SingleElectron/")>0:
@@ -55,9 +50,9 @@ else:
   json = JSON+'Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
   #json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
 
-ensureDir(outdir)
-
-outfile = "%s_%s_%s.root"%(outfile,nchunck,channel)
+tag = ""
+if tes!=1: tag +="_TES%.3f"%(tes)
+outfile = "%s_%s_%s%s.root"%(outfile,nchunck,channel,tag.replace('.','p'))
 postfix = "%s/%s"%(outdir,outfile)
 
 print '-'*80
@@ -88,9 +83,9 @@ elif channel=='mumu':
     from MuMuModule import *
     module2run = lambda : MuMuProducer(postfix, dataType, **kwargs)
 
-elif channel=='muele':
-    from MuEleModule import *
-    module2run = lambda : MuEleProducer(postfix, dataType)
+elif channel=='elemu':
+    from EleMuModule import *
+    module2run = lambda : EleMuProducer(postfix, dataType)
 else:
     print 'Unkonwn channel !!!'
     sys.exit(0)
